af63453e5eb65d5b4fe92273d66c6abc40145849
jiaoju.wu <690489417@qq.com>
2016-10-08 17:47:36 +0800

    1008
    1008

--- src/js/component/table.component.js
+++ src/js/component/table.component.js
@@ -1,9 +1,18 @@
 export function tableComponent(angular) {
     //然后 DataTables 这样初始化：
     angular.component("tableComponent", {
-        template: `<div ui-grid="gridOptions" ui-grid-edit ui-grid-cellnav class="grid"></div>`,
+        template : `<div ui-grid="gridOptions" ui-grid-edit ui-grid-cellnav class="grid"></div>`,
+        bindings : {
+            source : "@",
+            option : "@"
+        },
         controller: function ($scope,$http) {
-            $scope.gridOptions = {  };
+
+            let ctrl = this,
+                href = ctrl.source,
+                option = JSON.parse(ctrl.option);
+
+            $scope.gridOptions = option;
             $scope.gridOptions.enableCellEditOnFocus = true;
 
             $scope.gridOptions.columnDefs = [
@@ -13,19 +22,10 @@ export function tableComponent(angular) {
                 { name: 'name', displayName: 'Name (editOnFocus)', width: 200}
             ];
 
-            $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+            $http.get(href)
                 .success(function(data) {
                     $scope.gridOptions.data = data;
                 });
-
-            $scope.currentFocused = "";
-
-            $scope.getCurrentFocus = function(){
-                var rowCol = $scope.gridApi.cellNav.getFocusedCell();
-                if(rowCol !== null) {
-                    $scope.currentFocused = 'Row Id:' + rowCol.row.entity.id + ' col:' + rowCol.col.colDef.name;
-                }
-            }
 
             $scope.gridOptions.onRegisterApi = function(gridApi){
                 $scope.gridApi = gridApi;
